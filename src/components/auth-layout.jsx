84ce739e3b6b2a3bdcec867234d1bb2d4ee05a28
Prachi Shah <prachi_shah@momentum91.com>
2026-04-23 17:01:48 +0530

    Update AuthLayout component to use a new login image: replace page background pattern with a login image and adjust image styling for better fit.

--- src/components/auth-layout.jsx
+++ src/components/auth-layout.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
-import pagePattern from '@/assets/svgs/page-background-pattern.svg';
+import loginImage from '@/assets/images/login-img.jpeg';
 
-function AuthLayout({ children, imageSrc: imageSource = pagePattern, imageAlt = 'login' }) {
+function AuthLayout({ children, imageSrc: imageSource = loginImage, imageAlt = 'login' }) {
   return (
     <div className='h-screen bg-[var(--color-bg-weak-50)] w-full flex flex-col items-center'>
       <div className='w-full h-full flex rounded-[24px] justify-center items-center'>
         <div className='w-full h-full flex rounded-[24px] bg-white items-center'>
-          <div className='w-1/2 h-full p-10 relative flex flex-col items-center justify-center'>
+          <div className='w-1/2 h-full p-10 relative  flex flex-col items-center justify-center'>
             {children}
           </div>
 
-          <div className='w-1/2 flex h-full bg-[var(--color-bg-weak-50)]'>
-            <img
-              src={imageSource}
-              alt={imageAlt}
-              className='object-cover w-full h-full'
-            />
+          <div className='w-1/2 flex h-full '>
+            <img src={imageSource} alt={imageAlt} className='object-fit w-full h-full' />
           </div>
         </div>
       </div>
